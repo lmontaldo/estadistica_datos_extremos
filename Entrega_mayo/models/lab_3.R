505271399e5041e6552d37956ff1b88464e762ae
Laura Montaldo <montaldo.lau@gmail.com>
2024-04-09 22:28:46 -0300

    pot lab3

--- Entrega_mayo/models/lab_3.R
+++ Entrega_mayo/models/lab_3.R
@@ -1,5 +1,9 @@
+rm(list = ls())
 source("../extremales/Entrega_mayo/utils/libraries.R")
 source("../extremales/Entrega_mayo/utils/dpg.R")
+cat("--------------------------------------------------")
+cat("Distribucion de Pareto Generalizada (DPG)", '\n')
+cat("--------------------------------------------------")
 #
 xaux <- seq(0, 10, length = 10000)
 xi <- 0.2
@@ -8,13 +12,15 @@ mu <- 0
 #
 Fx <- dgp_distribution(xaux, xi, sigma, mu)
 # Plot the distribution with improved appearance
-plot(xaux, Fx, col = "red", type = "l", lwd = 2, ylim = c(0, 1), 
+dev.off()
+plot(xaux, Fx, col = "yellow", type = "l", lwd = 2, ylim = c(0, 1), 
      xlab = "x", ylab = "F(x)", main = "Distribucion de Pareto Generalizada (DPG)", 
      sub = "Parameters: xi = 0.2, sigma = 1, mu = 0")
-#
+
+
+dev.off()
 # Number of random combinations
 num_combinations <- 3
-
 # Set up the plotting layout
 par(mfrow = c(1, num_combinations))
 
@@ -29,9 +35,69 @@ for (i in 1:num_combinations) {
   Fx <- dgp_distribution(xaux, xi, sigma, mu)
   
   # Plot the distribution for the current combination
-  plot(xaux, Fx, col = "green", type = "l", lwd = 2, ylim = c(0, 1), 
+  plot(xaux, Fx, col = "yellow", type = "l", lwd = 2, ylim = c(0, 1), 
        xlab = "x", ylab = "F(x)", main = paste("Distribution Function (Combination", i, ")"), 
        sub = paste("Parameters: xi =", round(xi, 2), ", sigma =", round(sigma, 2), 
                    ", mu =", round(mu, 2)))
 }
 #
+dev.off()
+plot(xaux, pgpd(xaux, location=0, scale=1, shape=0.1),type="l", col="blue", ylim=c(0,1), ylab="F(x)", xlab="x", main= "Distribución de Pareto Generalizada")
+lines(xaux, pgpd(xaux, location=0, scale=1, shape=0),col="green")
+legend("bottomright", legend=c("xi=0 - Exponencial", "xi=0.1", "xi=2- pareto"), bty="n", col=c("green","blue","red"), lty=1)
+
+
+
+# funcion a mano
+xi=2
+sigma=1
+mu=0
+points(xaux, 1- (1+ (xi*(xaux-mu)/sigma))^(-1/xi),col="yellow")
+# función del VGAM
+lines(xaux, pgpd(xaux, location=0, scale=1, shape=2),col="red")
+#
+
+# Densidad (prestar atención a la diferencia con exponencial en las colas)
+plot(xaux, dgpd(xaux, location=0, scale=1, shape=2),type="l", col="red", log="", ylim=c(1e-5,1), ylab="f(x)", xlab="x")
+lines(xaux, dexp(xaux),col="green")
+lines(xaux, dgpd(xaux, location=0, scale=1, shape=0.1),col="blue")
+legend("topright", legend=c("xi=0 - Exponencial", "xi=0.1", "xi=2- pareto"), bty="n", col=c("green","blue","red"), lty=1)
+#
+# pongamos el eje Y en escala logaritmica para apreciar mejor el comportamiento en las colas
+plot(xaux, dgpd(xaux, location=0, scale=1, shape=2),type="l", col="red", log="y", ylim=c(1e-5,1), ylab="f(x)", xlab="x")
+lines(xaux, dexp(xaux),col="green")
+lines(xaux, dgpd(xaux, location=0, scale=1, shape=0.1),col="blue")
+legend("topright", legend=c("xi=0 - Exponencial", "xi=0.1", "xi=2- pareto"), bty="n", col=c("green","blue","red"), lty=1)
+#
+# Densidad (prestar atención a la diferencia con exponencial en las colas)
+xaux<-seq(0, 20, length=1000)
+plot(xaux, dgpd(xaux, location=0, scale=6, shape=0),type="l", col="green", ylab="f(x)", xlab="x")
+lines(xaux, dgpd(xaux, location=0, scale=6, shape=-1),col="orange")
+lines(xaux, dgpd(xaux, location=0, scale=6, shape=2),col="red")
+legend("topright", legend=c("xi=0 - Exponencial", "xi=-1 Uniforme", "xi=2- Pareto"), bty="n", col=c("green","orange","red"), lty=1)
+#
+set.seed(71)
+rgpd(20,location=0, scale=1, shape=2) #genero 70 números aleatorios de una variable X con DPG con esos parámetros
+#
+set.seed(233)
+plot(rgpd(20,location=0, scale=1, shape=2)) # notese la gran volatilidad pues el parámetro de forma es alto (xi=2)
+points(rgpd(20,location=0, scale=1, shape=0.2), col="green")
+dev.off()
+
+##################### 
+# POT
+#####################
+
+cat("Ejemplos aplicación de POT", "\n")
+set.seed(54)
+n=1500
+Obs<- rlnorm(n, 4,1) # muestra de entrenamiento. en mis datos lo debo partir
+plot(Obs,type="l")
+
+
+
+
+
+
+
+
